36d3b56465b343da0cb18005f4f21dd5839eb159
byverbel_wsl <byverbel@gmail.com>
2022-09-27 00:19:09 -0500

    changes notices in blog controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -25,7 +25,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to blog_url(@blog), notice: "Blog was successfully created." }
+        format.html { redirect_to blog_url(@blog), notice: "Se creó el blog correctamente." }
         format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class BlogsController < ApplicationController
   def update
     respond_to do |format|
       if @blog.update(blog_params)
-        format.html { redirect_to blog_url(@blog), notice: "Blog was successfully updated." }
+        format.html { redirect_to blog_url(@blog), notice: "Se editó el blog correctamente." }
         format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class BlogsController < ApplicationController
     @blog.destroy
 
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: "Blog was successfully destroyed." }
+      format.html { redirect_to blogs_url, notice: "Se borró el blog correctamente." }
       format.json { head :no_content }
     end
   end
