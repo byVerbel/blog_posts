74e52cd0a3cdf3a2d7af89e0e6302fc377be1e2c
byverbel_wsl <byverbel@gmail.com>
2022-09-27 18:05:12 -0500

    fixes bibliographies controller notices

--- app/controllers/bibliographies_controller.rb
+++ app/controllers/bibliographies_controller.rb
@@ -7,7 +7,7 @@ class BibliographiesController < ApplicationController
 
     respond_to do |format|
       if @bibliography.save
-        format.html { redirect_to blog_url(@bibliography.blog), notice: "Se creó la autoevaluación correctamente." }
+        format.html { redirect_to blog_url(@bibliography.blog), notice: "Se creó la referencia correctamente." }
         format.json { render :show, status: :created, location: @bibliography.blog }
       else
         format.html { redirect_to blog_url(@bibliography.blog), status: :unprocessable_entity }
@@ -20,7 +20,7 @@ class BibliographiesController < ApplicationController
   def update
     respond_to do |format|
       if @bibliography.update(bibliography_params)
-        format.html { redirect_to blog_url(@bibliography.blog), notice: "Se editó la autoevaluación correctamente." }
+        format.html { redirect_to blog_url(@bibliography.blog), notice: "Se editó la referencia correctamente." }
         format.json { render :show, status: :ok, location: @bibliography.blog }
       else
         format.html { redirect_to blog_url(@bibliography.blog), status: :unprocessable_entity }
@@ -35,7 +35,7 @@ class BibliographiesController < ApplicationController
     @bibliography.destroy
 
     respond_to do |format|
-      format.html { redirect_to blog_url(blog), notice: "Se borró la autoevaluación correctamente." }
+      format.html { redirect_to blog_url(blog), notice: "Se borró la referencia correctamente." }
       format.json { head :no_content }
     end
   end
