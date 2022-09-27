3c9352d43137517fc0f44468c8fd3905fb29ec13
byverbel_wsl <byverbel@gmail.com>
2022-09-27 00:46:34 -0500

    adds Blog associations with new models

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,4 @@
 class Blog < ApplicationRecord
+  has_one :apun_self_evaluation, class_name: "ApunSelfEvaluation", dependent: :destroy
+  has_many :bibliographies, class_name: "Bibliography", dependent: :destroy
 end
